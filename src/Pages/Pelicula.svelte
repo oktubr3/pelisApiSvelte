8de17813d5155d65ccb0dae89745fa23037fc261
oktubr3 <mauroh@gmail.com>
2021-11-14 01:11:16 -0300

    Ejercicio 40

--- src/Pages/Pelicula.svelte
+++ src/Pages/Pelicula.svelte
@@ -6,12 +6,12 @@
     const key = '?api_key=ad9a11d6e2c0d1628ab6cb4adbfeb14d&language=en-US';
     const urlFinal = url + id + key;
 
-    let peli = {};
+    let pelicula = {};
     let generos = [];
     onMount(async () => {
         const res = await fetch(urlFinal);
-        peli = res.json();
-        generos = await peli.genres;
+        pelicula = res.json();
+        generos = await pelicula.genres;
         console.log(pelicula);
         console.log(generos);
     });
