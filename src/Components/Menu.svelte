57ad9d056f2931e6a6a685ce185082c063f1636b
oktubr3 <mauroh@gmail.com>
2021-11-17 22:59:22 -0300

    Menu Corregido

--- src/Components/Menu.svelte
+++ src/Components/Menu.svelte
@@ -22,14 +22,17 @@
         <a href="/" class="font-semubold text-3xl pt-1 no-underline text-red-500 hover:text-red-300"><Icon class="pt-1 pb-2" data={film} scale="2.5"/> Movies & TV Shows</a>
     </div>
     <div class="flex">
-
-        <Switch bind:checked={tvShows}></Switch>
-
-            {#if tvShows}
-                {activeTvShow()}
-            {:else}
-                {activeMovies()}
-            {/if}
+        <div class="flex">
+        <h3>Movies</h3>
+        <Switch bind:checked={tvShows} />
+        <h3>TvShows</h3>
+            <span style="font-size: 0;">
+                {#if tvShows}
+                    {activeTvShow()}
+                {:else}
+                    {activeMovies()}
+                {/if}
+            </span>
     </div>
     </nav>
 </main>
